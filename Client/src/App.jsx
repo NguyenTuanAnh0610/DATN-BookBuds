931c820d862c4b86a4623367db48405303948e71
thanhnt04 <ntruongthanhdev04@gmail.com>
2025-04-24 01:05:18 +0700

    Tạo project client router

---
+++ Client/src/App.jsx
@@ -0,0 +1,27 @@
+'use client'
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import ProductPage from './components/ProductPage';
+
+function App() {
+    return (
+                <Router>
+                    <Header />
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/about" element={<About />} />
+                        <Route path="/contact" element={<Contact />} />
+                        <Route path="/products" element={<ProductPage />} />
+                    </Routes>
+                    <Footer />
+                </Router>
+    );
+}
+
+export default App;
