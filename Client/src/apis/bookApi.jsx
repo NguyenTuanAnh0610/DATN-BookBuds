f4336d5303f88f62c0bd82a5d7b40c87cec2cbe2
NguyenTuanAnh0610 <162524488+NguyenTuanAnh0610@users.noreply.github.com>
2025-04-26 22:18:07 +0700

    update home Client

---
+++ Client/src/apis/bookApi.jsx
@@ -0,0 +1,30 @@
+import axiosClient from './axiosClient';
+
+const bookApi = {
+    createBook(data) {
+        const url = '/books';
+        return axiosClient.post(url, data);
+    },
+    getDetailBook(id) {
+        const url = '/books/' + id;
+        return axiosClient.get(url);
+    },
+    getListBooks() {
+        const url = '/books';
+        return axiosClient.get(url);
+    },
+    getBooksByCategory(id) {
+        const url = '/books/category/' + id;
+        return axiosClient.get(url);
+    },
+    updateBook(id, data) {
+        const url = '/books/' + id;
+        return axiosClient.put(url, data);
+    },
+    deleteBook(id) {
+        const url = '/books/' + id;
+        return axiosClient.delete(url);
+    },
+}
+
+export default bookApi; 
