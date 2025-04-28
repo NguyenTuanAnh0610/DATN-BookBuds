a06729267e859b2deb7410375030c4e8da5c3da2
NguyenTuanAnh0610 <162524488+NguyenTuanAnh0610@users.noreply.github.com>
2025-04-28 22:34:06 +0700

    fix code

--- bookbuds/app/Http/Controllers/OrderController.php
+++ bookbuds/app/Http/Controllers/OrderController.php
@@ -81,17 +81,6 @@ class OrderController extends Controller
             $book->save();
         }
 
-        // Load the relationships needed for the email
-        $order->load(['orderItems.book', 'user', 'shippingAddress']);
-
-        // Send confirmation email
-        try {
-            Mail::to($order->user->email)->send(new OrderConfirmation($order));
-        } catch (\Exception $e) {
-            // Log the error but don't stop the order creation
-            \Log::error('Failed to send order confirmation email: ' . $e->getMessage());
-        }
-
         return response()->json($order->load(['orderItems.book']), 201);
     }
 
